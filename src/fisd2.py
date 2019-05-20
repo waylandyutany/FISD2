21b5c776cb028088408a6d9d1d109b7eb4b0854e
spirith <spirith@post.sk>
2019-05-20 14:15:08 +0200

    test_executed

--- src/fisd2.py
+++ src/fisd2.py
@@ -1,4 +1,10 @@
-import argparse
+import parser, sys
 from core.context import Context
 
 context = Context()
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    options = [str(arg).lower().strip(' -\t\n\r') for arg in args]
+    print(args)
+    print(options)
