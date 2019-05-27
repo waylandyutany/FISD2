e6a6fe1e77cbb8943064397b1d35281f11ccf1d2
spirith <spirith@post.sk>
2019-05-27 14:26:07 +0200

    another preparation for process execute statement

--- src/core/code.py
+++ src/core/code.py
@@ -1,23 +1,33 @@
 from core.tokens import Tokenizers, Tokens
 from default_commands.keywords import Keywords
 import os
+
 ################################################################################
 class Code:
     _FILES = 'files'
     _FUNCTIONS = 'functions'
 
+################################################################################
     def __init__(self):
         self.code = {Code._FILES:{}, Code._FUNCTIONS:{}}
         self.code_path = None
 
-    def find_fisd_file(self, file_name):
+################################################################################
+    def __find_fisd_file(self, file_name):
         if not self.code_path:
             self.code_path = os.path.dirname(file_name)
 
         return file_name, os.path.abspath(file_name)
 
+    def __process_execute_tokens(self, tokens, logger):
+        pass
+
+    def __process_tokens(self, tokens, logger):
+        self.__process_execute_tokens(tokens, logger)
+
+################################################################################
     def compile_from_file(self, file_name, logger):
-        file_name, file_path = self.find_fisd_file(file_name)
+        file_name, file_path = self.__find_fisd_file(file_name)
         if not file_name:
             return
         if file_name in self.code[Code._FILES]:
@@ -34,6 +44,8 @@ class Code:
 
                 tokens = Tokens(line)
                 Tokenizers.tokenize(tokens)
+
+                self.__process_tokens(tokens, logger)
 
                 if not tokens.empty():
                     self.code[Code._FILES][file_name][line_number] = tokens.tokens
