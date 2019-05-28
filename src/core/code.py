e4ffda03bc13c452f23ae03fd7b3bd1951e565d0
spirith <spirith@post.sk>
2019-05-28 11:57:24 +0200

    Tokenize files behind execute commands

--- src/core/code.py
+++ src/core/code.py
@@ -1,5 +1,6 @@
 from core.tokens import Tokenizers, Tokens
 from default_commands.keywords import Keywords
+
 import os
 
 ################################################################################
@@ -10,26 +11,43 @@ class Code:
 ################################################################################
     def __init__(self):
         self.code = {Code._FILES:{}, Code._FUNCTIONS:{}}
-        self.code_path = None
+        self.__code_path = None
+        self.__tokens_handlers = [self.__process_execute_tokens]
 
 ################################################################################
     def __find_fisd_file(self, file_name):
-        if not self.code_path:
-            self.code_path = os.path.dirname(file_name)
+        dir_name = os.path.dirname(file_name)
+        name, ext = os.path.splitext(os.path.basename(file_name))
 
-        return file_name, os.path.abspath(file_name)
+        possible_files = []
+        if len(dir_name) == 0:
+            dir_name = self.__code_path
+        if len(ext) == 0:
+            possible_files.append(os.path.join(dir_name, name) + '.fisd')
+            possible_files.append(os.path.join(dir_name, name) + '.fisd2')
+        else:
+            possible_files.append(os.path.join(dir_name, name) + ext)
+
+        for possible_file in possible_files:
+            if os.path.isfile(possible_file):
+                return os.path.basename(possible_file), os.path.abspath(possible_file)
+
+        return None, None
 
     def __process_execute_tokens(self, tokens, logger):
-        pass
-
-    def __process_tokens(self, tokens, logger):
-        self.__process_execute_tokens(tokens, logger)
+        if tokens.is_name(0) and tokens.is_value_no_case(0, Keywords._EXECUTE):
+            if not tokens.is_string(1):
+                logger.error("Valid fisd file name must follow 'execute' command!")
+                return
+            self.__tokenize_from_file(tokens.value_str(1), logger)
 
 ################################################################################
-    def compile_from_file(self, file_name, logger):
-        file_name, file_path = self.__find_fisd_file(file_name)
+    def __tokenize_from_file(self, _file_name, logger):
+        file_name, file_path = self.__find_fisd_file(_file_name)
         if not file_name:
+            logger.error("Non existing file '{}'!".format(_file_name))
             return
+
         if file_name in self.code[Code._FILES]:
             return
         self.code[Code._FILES][file_name] = {}
@@ -45,10 +63,14 @@ class Code:
                 tokens = Tokens(line)
                 Tokenizers.tokenize(tokens)
 
-                self.__process_tokens(tokens, logger)
+                for tokens_handler in self.__tokens_handlers:
+                    tokens_handler(tokens, logger)
 
                 if not tokens.empty():
                     self.code[Code._FILES][file_name][line_number] = tokens.tokens
                     logger.info("{}".format(tokens.tokens))
 
-        return True
+################################################################################
+    def compile_from_file(self, file_name, logger):
+        self.__code_path = os.path.dirname(file_name)
+        self.__tokenize_from_file(file_name, logger)
--- src/core/tokens.py
+++ src/core/tokens.py
@@ -25,38 +25,52 @@ def tokenizer_class(name):
 class Tokens:
     valid_tokens = [TOKEN_NAME, TOKEN_STRING, TOKEN_NUMBER, TOKEN_OP]
 
+################################################################################
     def __init__(self, line, encoding = 'utf-8'):
         self._tokens = [[tok_id, tok_val] for tok_id, tok_val, _, _, _  in tokenize(BytesIO(line.encode(encoding)).readline) if tok_id in Tokens.valid_tokens]
 
-    def insert_token(self, index, type, value):
-        self._tokens.insert(index, [type, value])
-
-    def empty(self):
-        return len(self._tokens) == 0
-
-    def token(self, index, tindex):
+################################################################################
+    def __token(self, index, tindex):
         try:
             return self._tokens[index][tindex]
         except:
             pass
         return None
 
+################################################################################
+    def insert_token(self, index, type, value):
+        self._tokens.insert(index, [type, value])
+
+    def empty(self):
+        return len(self._tokens) == 0
+
     @property
     def tokens(self):
         return tuple(self._tokens)
 
+################################################################################
     def is_name(self, index):
-        return self.token(index, 0) == TOKEN_NAME
+        return self.__token(index, 0) == TOKEN_NAME
 
     def is_number(self, index):
-        return self.token(index, 0) == TOKEN_NUMBER
+        return self.__token(index, 0) == TOKEN_NUMBER
 
     def is_string(self, index):
-        return self.token(index, 0) == TOKEN_STRING
+        return self.__token(index, 0) == TOKEN_STRING
 
     def is_op(self, index):
-        return self.token(index, 0) == TOKEN_OP
+        return self.__token(index, 0) == TOKEN_OP
 
     def is_value(self, index, value):
-        return self.token(index, 1) == value
+        return self.__token(index, 1) == value
 
+    def is_value_no_case(self, index, value):
+        return str(self.__token(index, 1)).lower() == str(value).lower()
+
+    def value(self, index):
+        return self.__token(index, 1)
+
+    def value_str(self, index):
+        if self.is_string(index):
+            return self.__token(index, 1)[1:-1]
+        return str(self.__token(index, 1))
