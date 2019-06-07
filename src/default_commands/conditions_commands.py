4753752f321e9c7501d910499334dd7aa842d594
spirith <spirith@post.sk>
2019-06-07 12:07:34 +0200

    elif optimization

--- src/default_commands/conditions_commands.py
+++ src/default_commands/conditions_commands.py
@@ -45,22 +45,21 @@ class IfCommand(Command):
 
         #set _KEY_NEXT_CONDITION_CODE_INDEX for all conditional commands, except endif
         for i in range(0, len(if_commands) - 1):
+            _, line_tokens, _ = code_utils.split_code_line(parse_args.code_lines[i])
+
             code_index = if_commands[i]
             next_condition_code_index = if_commands[i + 1]
-            end_condition_code_index = if_commands[-1]
             parse_args.code_lines[code_index][IfCommand._KEY_NEXT_CONDITION_CODE_INDEX] = next_condition_code_index
-            parse_args.code_lines[code_index][IfCommand._KEY_END_CONDITION_CODE_INDEX] = end_condition_code_index
+    
+            # _KEY_END_CONDITION_CODE_INDEX optimization apply only for elseif statement
+            if line_tokens.is_value_no_case(0, Keywords._ELIF):
+                end_condition_code_index = if_commands[-1]
+                parse_args.code_lines[code_index][IfCommand._KEY_END_CONDITION_CODE_INDEX] = end_condition_code_index
         
     @classmethod
     def execute(cls, execute_args):
         # must be first !!!
         execute_args.context.begin_if()
-
-        # jump to the if end when condition already satisfied, preventing condition statement evaluation
-        if execute_args.context.is_skip_if():
-            end_condition_code_index = execute_args.code_line[IfCommand._KEY_END_CONDITION_CODE_INDEX]
-            execute_args.context.jump_to_code(end_condition_code_index)
-            return
 
         result = code_utils.evaluate_tokens(execute_args.arguments, 0, len(execute_args.arguments) - 1)
 
@@ -95,8 +94,8 @@ class ElseProcCommand(Command):
     @classmethod
     def execute(cls, execute_args):
         if execute_args.context.is_skip_if():
-            end_condition_code_index = execute_args.code_line[IfCommand._KEY_END_CONDITION_CODE_INDEX]
-            execute_args.context.jump_to_code(end_condition_code_index)
+            next_condition_code_index = execute_args.code_line[IfCommand._KEY_NEXT_CONDITION_CODE_INDEX]
+            execute_args.context.jump_to_code(next_condition_code_index)
 
 ################################################################################
 # ELIF Command
@@ -111,4 +110,10 @@ class ElifProcCommand(Command):
 
     @classmethod
     def execute(cls, execute_args):
+        # jump to the if end when condition already satisfied, preventing condition statement evaluation
+        if execute_args.context.is_skip_if():
+            end_condition_code_index = execute_args.code_line[IfCommand._KEY_END_CONDITION_CODE_INDEX]
+            execute_args.context.jump_to_code(end_condition_code_index)
+            return
+
         IfCommand.execute(execute_args)
