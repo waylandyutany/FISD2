4b6b6f5b20317b7219989e3fbb9340f1b1aa58a6
spirith <spirith@post.sk>
2019-06-22 15:11:09 +0200

    todos

--- src/core/context.py
+++ src/core/context.py
@@ -8,7 +8,7 @@ import json
 
 ################################################################################
 class ExecuteArgs: 
-    #@ properties for tokens, ..., code_line is property as well, for Parse arguments as well !!!
+    #@ properties for tokens, ..., code_line is property as well, for Parse arguments as well !!! optimize second call of property !!!
     def __init__(self, _context, _logger, _code_name, _code_lines):
         self.code_name = _code_name
         self.code_lines = _code_lines
