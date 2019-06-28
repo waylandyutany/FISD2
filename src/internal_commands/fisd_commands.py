8c4ea18663d793546de3f0b1050fa26ce74e6396
spirith <spirith@post.sk>
2019-06-28 16:37:41 +0200

    cleanups in fisd_commands.py

--- src/internal_commands/fisd_commands.py
+++ src/internal_commands/fisd_commands.py
@@ -4,74 +4,44 @@ from core.commands import command_class, Command
 ################################################################################
 # FISD_STORE_CONTEXT Command
 ################################################################################
-@command_class()
+@command_class('fisd_store_context')
 class Fisd_store_context_command(Command):
-    _keyword = 'fisd_store_context'
-
     @staticmethod
-    def parse(pargs):
-        pass    
-
-    @staticmethod
-    def execute(eargs):
-        eargs.context.store_context(eargs.evaluated_args.value(0))
+    def execute(params):
+        params.context.store_context(params.evaluated_args.value(0))
 
 ################################################################################
 # FISD_RESTORE_CONTEXT Command
 ################################################################################
-@command_class()
+@command_class('fisd_restore_context')
 class Fisd_restore_context_command(Command):
-    _keyword = 'fisd_restore_context'
-
     @staticmethod
-    def parse(pargs):
-        pass    
-
-    @staticmethod
-    def execute(eargs):
+    def execute(params):
         pass
 
 ################################################################################
 # FISD_CURRENT_FOLDER Command
 ################################################################################
-@command_class()
+@command_class('fisd_current_folder')
 class Fisd_current_folder_command(Command):
-    _keyword = 'fisd_current_folder'
-
     @staticmethod
-    def parse(pargs):
-        pass    
-
-    @staticmethod
-    def execute(eargs):
-        eargs.context.set_return_value(eargs.context.execution.current_file_folder())
+    def execute(params):
+        params.context.set_return_value(params.context.execution.current_file_folder())
 
 ################################################################################
 # FISD_CURRENT_FILE Command
 ################################################################################
-@command_class()
+@command_class('fisd_current_file')
 class Fisd_current_file_command(Command):
-    _keyword = 'fisd_current_file'
-
     @staticmethod
-    def parse(pargs):
-        pass    
-
-    @staticmethod
-    def execute(eargs):
-        eargs.context.set_return_value(eargs.context.execution.current_file_name())
+    def execute(params):
+        params.context.set_return_value(params.context.execution.current_file_name())
 
 ################################################################################
 # FISD_LINE_NUMBER Command
 ################################################################################
-@command_class()
+@command_class('fisd_line_number')
 class Fisd_line_number_command(Command):
-    _keyword = 'fisd_line_number'
-
     @staticmethod
-    def parse(pargs):
-        pass    
-
-    @staticmethod
-    def execute(eargs):
-        eargs.context.set_return_value(eargs.context.execution.current_line_number())
+    def execute(params):
+        params.context.set_return_value(params.context.execution.current_line_number())
