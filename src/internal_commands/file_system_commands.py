145e139dfb89e024b582d3660e407e9ab8199044
spirith <spirith@post.sk>
2019-07-02 11:42:33 +0200

    minor stuff

--- src/internal_commands/file_system_commands.py
+++ src/internal_commands/file_system_commands.py
@@ -3,8 +3,9 @@ from core.commands import command_class, Command
 ################################################################################
 # FILE_COUNT_LINE Command
 ################################################################################
-@command_class('file_count_line')
-class File_count_lineCommand(Command):
+@command_class('file_count_lines')
+class File_count_linesCommand(Command):
     @classmethod
     def execute(cls, params):
         params.logger.info("{}({})".format(cls._keyword, str(params.raw_args)))
+        params.set_return(0)
