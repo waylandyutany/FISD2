f1aba0e3aeab65babdf210f9729cc6e55cd3780d
spirith <spirith@post.sk>
2019-07-07 14:44:16 +0200

    Counting passed and failed assertions

--- src/testing/testing_stat.py
+++ src/testing/testing_stat.py
@@ -6,6 +6,8 @@ class TestingStat:
     _key_test_sets = 'test_sets'
     _key_test_cases = 'test_cases'
     _key_description = 'description'
+    _key_passed = 'passed'
+    _key_failed = 'failed'
 
     def __init__(self, logger):
         self.__logger = logger
@@ -46,8 +48,19 @@ class TestingStat:
 
         if self.__tc_name not in test_cases:
             test_cases[self.__tc_name] = {}
+            TestingStat.__reset_tc_node(test_cases[self.__tc_name])
 
         return test_cases[self.__tc_name]
+
+    @classmethod
+    def __reset_tc_node(cls, tc_node):
+        tc_node[cls._key_passed] = 0
+        tc_node[cls._key_failed] = 0
+
+    @classmethod
+    def __increment_tc_node(cls, tc_node, passed, failed):
+        tc_node[cls._key_passed] += passed
+        tc_node[cls._key_failed] += failed
 
 ################################################################################
     def begin_test_suite(self, name, description):
@@ -67,6 +80,10 @@ class TestingStat:
         node = self.__get_node(self.__suite_name, self.__set_name, name)
         if description:
             node[TestingStat._key_description] = description
+        TestingStat.__reset_tc_node(node)
         
     def check_assert(self, place, evaluation, evaluation_string, evaluation_description):
-        pass
+        node = self.__get_node(self.__suite_name, self.__set_name, self.__tc_name)
+        passed = 1 if (evaluation == True) else 0
+        failed = 1 if (evaluation == False) else 0
+        TestingStat.__increment_tc_node(node, passed, failed)
