4ea85e7ba278946f1a689ba84d6ccf13439b37c3
spirith <spirith@post.sk>
2019-07-08 12:42:29 +0200

    testing_stat is using system variables

--- src/core/execution/system_variables.py
+++ src/core/execution/system_variables.py
@@ -10,3 +10,11 @@ class SystemVariables:
 
     def from_json_dict(self, json_dict):
         self.__variables = json_dict['variables']
+
+    def set(self, name, value):
+        self.__variables[name] = value
+
+    def get(self, name, default_value = None):
+        if name in self.__variables:
+            return self.__variables[name]
+        return default_value
--- src/testing/testing_stat.py
+++ src/testing/testing_stat.py
@@ -31,12 +31,13 @@ class TestingStat:
     _key_description = 'description'
     _key_passed = 'passed'
     _key_failed = 'failed'
+    _key_test_suite = 'test_suite'
+    _key_test_set = 'test_set'
+    _key_test_case = 'test_case'
+    _key_default = 'default'
 
     def __init__(self, logger):
         self.__logger = logger
-        self.__suite_name = 'default_suite' #@todo or None instead ?
-        self.__set_name = 'default_set'
-        self.__tc_name = 'default_tc'
         self.__stat = {TestingStat._key_test_suites:{}}
 
     def save(self, file_path):
@@ -83,11 +84,11 @@ class TestingStat:
         if tc_name == None:
             return test_cases
 
-        if self.__tc_name not in test_cases:
-            test_cases[self.__tc_name] = {}
-            TestingStat.__reset_tc_node(test_cases[self.__tc_name])
+        if tc_name not in test_cases:
+            test_cases[tc_name] = {}
+            TestingStat.__reset_tc_node(test_cases[tc_name])
 
-        return test_cases[self.__tc_name]
+        return test_cases[tc_name]
 
     @classmethod
     def __reset_tc_node(cls, tc_node):
@@ -101,26 +102,28 @@ class TestingStat:
 
 ################################################################################
     def begin_test_suite(self, system_var, name, description):
-        self.__suite_name = name
+        system_var.set(TestingStat._key_test_suite, name)
         node = self.__get_node(name)
-        if description:
-            node[TestingStat._key_description] = description
+        #node[TestingStat._key_description] = description
 
     def begin_test_set(self, system_var, name, description):
-        self.__set_name = name
-        node = self.__get_node(self.__suite_name, name)
-        if description:
-            node[TestingStat._key_description] = description
+        system_var.set(TestingStat._key_test_set, name)
+        node = self.__get_node(system_var.get(TestingStat._key_test_suite, TestingStat._key_default),
+                               name)
+        #node[TestingStat._key_description] = description
 
     def begin_test_case(self, system_var, name, description):
-        self.__tc_name = name
-        node = self.__get_node(self.__suite_name, self.__set_name, name)
-        if description:
-            node[TestingStat._key_description] = description
+        system_var.set(TestingStat._key_test_case, name)
+        node = self.__get_node(system_var.get(TestingStat._key_test_suite, TestingStat._key_default),
+                               system_var.get(TestingStat._key_test_set, TestingStat._key_default),
+                               name)
+        node[TestingStat._key_description] = description
         TestingStat.__reset_tc_node(node)
         
     def check_assert(self, system_var, place, evaluation, evaluation_string, evaluation_description):
-        node = self.__get_node(self.__suite_name, self.__set_name, self.__tc_name)
+        node = self.__get_node(system_var.get(TestingStat._key_test_suite, TestingStat._key_default),
+                               system_var.get(TestingStat._key_test_set, TestingStat._key_default),
+                               system_var.get(TestingStat._key_test_case, TestingStat._key_default))
         passed = 1 if (evaluation == True) else 0
         failed = 1 if (evaluation == False) else 0
         TestingStat.__increment_tc_node(node, passed, failed)
