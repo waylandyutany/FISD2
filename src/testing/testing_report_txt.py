93da88a24bbf5072456c6cbfcf8e344e1aaf2101
spirith <spirith@post.sk>
2019-07-09 11:50:49 +0200

    Basic text testing report implemented

--- src/testing/testing_report_txt.py
+++ src/testing/testing_report_txt.py
@@ -1,22 +1,33 @@
 from testing.testing_report import TestingReport, testing_report_class
+from core.safe_utils import safe_path
 
 ################################################################################
 @testing_report_class('.txt')
 class TestingReport_TXT(TestingReport):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.txt_lines = []
+        self.txt_lines.append("TEST_REPORT('{}')".format(self.report_file_path))
+
         self.test_stat.enumerate(self)
 
+        with open(safe_path(self.report_file_path), 'w') as f:
+            for txt_line in self.txt_lines:
+                print(txt_line)
+                f.write(txt_line + "\n")
+        
+
     def on_test_suite(self, test_suite_info):
-#        print("on_test_suite : ", test_suite_info.name)
-        pass
+        self.txt_lines.append("TEST_SUITE('{}')".format(test_suite_info.name))
 
     def on_test_set(self, test_set_info):
-#        print("on_test_set : ", test_set_info.name)
-        pass
+        self.txt_lines.append("    TEST_SET('{}')".format(test_set_info.name))
 
     def on_test_case(self, test_case_info):
-#        print("on_test_case : ", test_case_info.name)
-        pass
+        if test_case_info.failed_assertions > 0:
+            self.txt_lines.append("        [FAIL]TEST_CASE('{}')".format(test_case_info.name))
+        else:
+            self.txt_lines.append("        [PASS]TEST_CASE('{}')".format(test_case_info.name))
 
 
