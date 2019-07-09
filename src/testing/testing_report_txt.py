b2725cb234da8725cae7d4b2a382c525a77a49e9
spirith <spirith@post.sk>
2019-07-09 12:11:37 +0200

    Improved txt test report

--- src/testing/testing_report_txt.py
+++ src/testing/testing_report_txt.py
@@ -8,7 +8,7 @@ class TestingReport_TXT(TestingReport):
         super().__init__(*args, **kwargs)
 
         self.txt_lines = []
-        self.txt_lines.append("TEST_REPORT('{}')".format(self.report_file_path))
+        self.txt_lines.append("TEST_REPORT - '{}'".format(self.report_file_path))
 
         self.test_stat.enumerate(self)
 
@@ -19,15 +19,21 @@ class TestingReport_TXT(TestingReport):
         
 
     def on_test_suite(self, test_suite_info):
-        self.txt_lines.append("TEST_SUITE('{}')".format(test_suite_info.name))
+        description = "" if not test_suite_info.description else " - '{}'".format(test_suite_info.description)
+        self.txt_lines.append("TEST_SUITE - '{}'".format(test_suite_info.name) + description)
 
     def on_test_set(self, test_set_info):
-        self.txt_lines.append("    TEST_SET('{}')".format(test_set_info.name))
+        description = "" if not test_set_info.description else " - '{}'".format(test_set_info.description)
+        self.txt_lines.append("    TEST_SET - '{}'".format(test_set_info.name) + description)
 
     def on_test_case(self, test_case_info):
+        description = "" if not test_case_info.description else " - '{}'".format(test_case_info.description)
         if test_case_info.failed_assertions > 0:
-            self.txt_lines.append("        [FAIL]TEST_CASE('{}')".format(test_case_info.name))
+            self.txt_lines.append("        TEST_CASE - FAIL - '{}'".format(test_case_info.name) + description)
+            for failure in test_case_info.failures:
+                what = "" if not failure.what else " - '{}'".format(failure.what)
+                self.txt_lines.append("            FAILURE - '{where}' - '{why}'{what}".format(what = what, why = failure.why, where = failure.where))
         else:
-            self.txt_lines.append("        [PASS]TEST_CASE('{}')".format(test_case_info.name))
+            self.txt_lines.append("        TEST_CASE - PASS - '{}'".format(test_case_info.name) + description)
 
 
