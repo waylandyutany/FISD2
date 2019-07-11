336409895e4df33d81093d1bf64e8754b3e2845d
spirith <spirith@post.sk>
2019-07-11 13:06:36 +0200

    todos

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -1,1 +1,1 @@
-python.exe "../src/fisd2.py" --fisd-file ../test/**/tc_*.fisd2 ../test/**/tc_*.bin --test-report-file ../reports/report.txt
+python.exe "../src/fisd2.py" --fisd-file ../test/**/tc_*.fisd2 ../test/**/tc_*.bin --test-report-file reports/report.txt
